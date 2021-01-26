76777bcd1830e8c670bf2ab0ade4e38fd3719f27
GachinAndrey <gachin.andrei@gmail.com>
2021-01-26 15:33:48 +0300

    init

---
+++ 001.py
@@ -0,0 +1,12 @@
+def salary():
+    try:
+        work_time = float(input('Выработка часов: '))
+        rate = float(input('Ставка з/п: '))
+        bonus = float(input('Премия: '))
+        result = work_time * rate + bonus
+        print(f'Заработная плата сотрудника:  {result}')
+    except ValueError:
+        return print('Введено не число!')
+
+
+salary()
